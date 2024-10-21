e81fb00e049c9a859bac22ee05a011d54e247e53
Jayant R Habbu <habbujayanth@gmail.com>
2024-10-21 18:27:48 +0530

    Create  Split a String Into the Max Number of Unique Substrings.java
    Given a string s, return the maximum number of unique substrings that the given string can be split into.
    
    You can split string s into any list of non-empty substrings, where the concatenation of the substrings forms the original string. However, you must split the substrings such that all of them are unique.
    
    A substring is a contiguous sequence of characters within a string.

---
+++ Strings/Split a String Into the Max Number of Unique Substrings.java
@@ -0,0 +1,40 @@
+// Solution
+class Solution {
+    public int maxUniqueSplit(String s) {
+        return backtrack(0, s, new HashSet<>());
+    }
+    private int backtrack(int start, String s, HashSet<String> seen) {
+        if (start == s.length()) {
+            return 0; 
+        }
+        int maxSplits = 0;
+        for (int end = start + 1; end <= s.length(); end++) {
+            String substring = s.substring(start, end);
+            if (!seen.contains(substring)) {
+                seen.add(substring); 
+                maxSplits = Math.max(maxSplits, 1 + backtrack(end, s, seen));
+                seen.remove(substring);
+            }
+        }
+        return maxSplits;
+    }
+}
+
+/*
+
+Example 1:
+Input: s = "ababccc"
+Output: 5
+Explanation: One way to split maximally is ['a', 'b', 'ab', 'c', 'cc']. Splitting like ['a', 'b', 'a', 'b', 'c', 'cc'] is not valid as you have 'a' and 'b' multiple times.
+
+Example 2:
+Input: s = "aba"
+Output: 2
+Explanation: One way to split maximally is ['a', 'ba'].
+
+Example 3:
+Input: s = "aa"
+Output: 1
+Explanation: It is impossible to split the string any further.
+
+*/
