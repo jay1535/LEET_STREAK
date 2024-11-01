68a11f25325027b78e1bfbda81235300f272d1cc
Jayant R Habbu <habbujayanth@gmail.com>
2024-11-01 12:45:20 +0530

    Create Delete Characters to Make Fancy String.java
    A fancy string is a string where no three consecutive characters are equal.
    
    Given a string s, delete the minimum possible number of characters from s to make it fancy.
    
    Return the final string after the deletion. It can be shown that the answer will always be unique.

---
+++ Strings/Delete Characters to Make Fancy String.java
@@ -0,0 +1,36 @@
+// Solution
+
+class Solution {
+    public String makeFancyString(String s) {
+        StringBuilder result = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            int n = result.length();
+            if (n < 2 || !(result.charAt(n - 1) == c && result.charAt(n - 2) == c)) {
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
+}
+
+/* 
+Example 1:
+Input: s = "leeetcode"
+Output: "leetcode"
+Explanation:
+Remove an 'e' from the first group of 'e's to create "leetcode".
+No three consecutive characters are equal, so return "leetcode".
+
+Example 2:
+Input: s = "aaabaaaa"
+Output: "aabaa"
+Explanation:
+Remove an 'a' from the first group of 'a's to create "aabaaaa".
+Remove two 'a's from the second group of 'a's to create "aabaa".
+No three consecutive characters are equal, so return "aabaa".
+
+Example 3:
+Input: s = "aab"
+Output: "aab"
+Explanation: No three consecutive characters are equal, so return "aab".
+*/
