907aef0904142c40bd6c6b4019f19c2ed8421f30
Jayant R Habbu <habbujayanth@gmail.com>
2024-11-03 08:08:19 +0530

    Create Rotate String.java
    Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
    
    A shift on s consists of moving the leftmost character of s to the rightmost position.
    
    For example, if s = "abcde", then it will be "bcdea" after one shift.

---
+++ Strings/Rotate String.java
@@ -0,0 +1,23 @@
+// Solution
+
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        return (s + s).contains(goal);
+    }
+}
+
+/* 
+
+Example 1:
+Input: s = "abcde", goal = "cdeab"
+Output: true
+
+Example 2:
+Input: s = "abcde", goal = "abced"
+Output: false
+ 
+
+*/
