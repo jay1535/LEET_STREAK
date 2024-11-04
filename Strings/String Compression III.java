9c4650ea6dcd197f9b9a1f90bd7a77dd8292e493
Jayant R Habbu <habbujayanth@gmail.com>
2024-11-04 08:34:03 +0530

    Create String Compression III.java
    Given a string word, compress it using the following algorithm:
    
    Begin with an empty string comp. While word is not empty, use the following operation:
    Remove a maximum length prefix of word made of a single character c repeating at most 9 times.
    Append the length of the prefix followed by c to comp.
    Return the string comp.

---
+++ Strings/String Compression III.java
@@ -0,0 +1,43 @@
+// Solution
+
+class Solution {
+    public String compressedString(String word) {
+        StringBuilder comp = new StringBuilder();
+        int cnt = 1, n = word.length();
+        char ch = word.charAt(0);
+        for (int i = 1; i < n; i++) {
+            if (word.charAt(i) == ch && cnt < 9) {
+                cnt++;
+            } else {
+                comp.append(cnt).append(ch);
+                ch = word.charAt(i);
+                cnt = 1;
+            }
+        }
+        comp.append(cnt).append(ch);
+        return comp.toString();
+    }
+}
+
+
+/*
+
+Example 1:
+Input: word = "abcde"
+Output: "1a1b1c1d1e"
+
+Explanation:
+Initially, comp = "". Apply the operation 5 times, choosing "a", "b", "c", "d", and "e" as the prefix in each operation.
+For each prefix, append "1" followed by the character to comp.
+
+Example 2:
+Input: word = "aaaaaaaaaaaaaabb"
+Output: "9a5a2b"
+
+Explanation:
+Initially, comp = "". Apply the operation 3 times, choosing "aaaaaaaaa", "aaaaa", and "bb" as the prefix in each operation.
+For prefix "aaaaaaaaa", append "9" followed by "a" to comp.
+For prefix "aaaaa", append "5" followed by "a" to comp.
+For prefix "bb", append "2" followed by "b" to comp.
+
+*/
