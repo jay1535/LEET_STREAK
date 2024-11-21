54fa5957ae8089cf1e76621d63c8de5b754c2bc8
Jayant R Habbu <habbujayanth@gmail.com>
2024-11-21 08:46:21 +0530

    Create Count Unguarded Cells in the Grid.java
    You are given two integers m and n representing a 0-indexed m x n grid. You are also given two 2D integer arrays guards and walls where guards[i] = [rowi, coli] and walls[j] = [rowj, colj] represent the positions of the ith guard and jth wall respectively.
    
    A guard can see every cell in the four cardinal directions (north, east, south, or west) starting from their position unless obstructed by a wall or another guard. A cell is guarded if there is at least one guard that can see it.
    
    Return the number of unoccupied cells that are not guarded.

---
+++ Arrays/Count Unguarded Cells in the Grid.java
@@ -0,0 +1,52 @@
+//Solution
+
+class Solution {
+    public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
+
+        int[][] g = new int[m][n];
+        for (int[] e : guards) {
+            g[e[0]][e[1]] = 2;
+        }
+        for (int[] e : walls) {
+            g[e[0]][e[1]] = 2;
+        }
+        int[] dirs = {-1, 0, 1, 0, -1};
+        for (int[] e : guards) {
+            for (int k = 0; k < 4; ++k) {
+                int x = e[0], y = e[1];
+                int dx = dirs[k], dy = dirs[k + 1];
+                while (x + dx >= 0 && x + dx < m && y + dy >= 0 && y + dy < n && g[x + dx][y + dy] < 2) {
+                    x += dx;
+                    y += dy;
+                    g[x][y] = 1;
+                }
+            }
+        }
+        int unguardedCount = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (g[i][j] == 0) {
+                    unguardedCount++;
+                }
+            }
+        }
+        
+        return unguardedCount;
+    }
+}
+
+/*
+
+Example 1:
+Input: m = 4, n = 6, guards = [[0,0],[1,1],[2,3]], walls = [[0,1],[2,2],[1,4]]
+Output: 7
+Explanation: The guarded and unguarded cells are shown in red and green respectively in the above diagram.
+There are a total of 7 unguarded cells, so we return 7.
+
+Example 2:
+Input: m = 3, n = 3, guards = [[1,1]], walls = [[0,1],[1,0],[2,1],[1,2]]
+Output: 4
+Explanation: The unguarded cells are shown in green in the above diagram.
+There are a total of 4 unguarded cells, so we return 4.
+ 
+*/
