e7b8b9526a7c3dbb7b9cf3fb1024023d922cbaae
Jayant R Habbu <habbujayanth@gmail.com>
2024-12-01 21:21:11 +0530

    Create Check If N and Its Double Exist.java
    Given an array arr of integers, check if there exist two indices i and j such that :
    
    i != j
    0 <= i, j < arr.length
    arr[i] == 2 * arr[j]

---
+++ Arrays/HashMap/Check If N and Its Double Exist.java
@@ -0,0 +1,33 @@
+// Solution
+
+import java.util.HashSet;
+
+public class Solution {
+    public boolean checkIfExist(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : arr) {
+            if (set.contains(num * 2) || (num % 2 == 0 && set.contains(num / 2))) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
+
+
+/*
+
+Example 1:
+
+Input: arr = [10,2,5,3]
+Output: true
+Explanation: For i = 0 and j = 2, arr[i] == 10 == 2 * 5 == 2 * arr[j]
+Example 2:
+
+Input: arr = [3,1,7,11]
+Output: false
+Explanation: There is no i and j that satisfy the conditions.
+ 
+
+*/
