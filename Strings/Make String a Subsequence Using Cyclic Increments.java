4108d212bafb27febd789d1a23e3596d28fa8560
Jayant R Habbu <habbujayanth@gmail.com>
2024-12-04 19:02:39 +0530

    Create Make String a Subsequence Using Cyclic Increments.java
    You are given two 0-indexed strings str1 and str2.
    
    In an operation, you select a set of indices in str1, and for each index i in the set, increment str1[i] to the next character cyclically. That is 'a' becomes 'b', 'b' becomes 'c', and so on, and 'z' becomes 'a'.
    
    Return true if it is possible to make str2 a subsequence of str1 by performing the operation at most once, and false otherwise.
    
    Note: A subsequence of a string is a new string that is formed from the original string by deleting some (possibly none) of the characters without disturbing the relative positions of the remaining characters.

---
+++ Strings/Make String a Subsequence Using Cyclic Increments.java
@@ -0,0 +1,40 @@
+// Solution
+
+class Solution {
+    public boolean canMakeSubsequence(String source, String target) {
+        int targetIdx = 0, targetLen = target.length();
+        
+        for (char currChar : source.toCharArray()) {
+            if (targetIdx < targetLen && (target.charAt(targetIdx) - currChar + 26) % 26 <= 1) {
+                targetIdx++;
+            }
+        }
+        return targetIdx == targetLen;
+    }
+}
+
+/*
+
+Example 1:
+Input: str1 = "abc", str2 = "ad"
+Output: true
+Explanation: Select index 2 in str1.
+Increment str1[2] to become 'd'. 
+Hence, str1 becomes "abd" and str2 is now a subsequence. Therefore, true is returned.
+
+Example 2:
+Input: str1 = "zc", str2 = "ad"
+Output: true
+Explanation: Select indices 0 and 1 in str1. 
+Increment str1[0] to become 'a'. 
+Increment str1[1] to become 'd'. 
+Hence, str1 becomes "ad" and str2 is now a subsequence. Therefore, true is returned.
+
+Example 3:
+Input: str1 = "ab", str2 = "d"
+Output: false
+Explanation: In this example, it can be shown that it is impossible to make str2 a subsequence of str1 using the operation at most once. 
+Therefore, false is returned.
+ 
+
+*/
