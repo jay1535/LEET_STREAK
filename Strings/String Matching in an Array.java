595714a537274fcc6b58c160695cdd36af672e35
Jayant R Habbu <habbujayanth@gmail.com>
2025-01-07 09:38:52 +0530

    Create String Matching in an Array.java
    Given an array of string words, return all strings in words that is a substring of another word. You can return the answer in any order.
    
    A substring is a contiguous sequence of characters within a string

---
+++ Strings/String Matching in an Array.java
@@ -0,0 +1,41 @@
+//Solution
+
+import java.util.*;
+
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        int n = words.length;
+        List<String> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i != j && words[j].contains(words[i])) {
+                    ans.add(words[i]);
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
+
+/*
+
+Example 1:
+Input: words = ["mass","as","hero","superhero"]
+Output: ["as","hero"]
+Explanation: "as" is substring of "mass" and "hero" is substring of "superhero".
+["hero","as"] is also a valid answer.
+
+Example 2:
+Input: words = ["leetcode","et","code"]
+Output: ["et","code"]
+Explanation: "et", "code" are substring of "leetcode".
+
+Example 3:
+Input: words = ["blue","green","bu"]
+Output: []
+Explanation: No string of words is substring of another string.
+ 
+*/
