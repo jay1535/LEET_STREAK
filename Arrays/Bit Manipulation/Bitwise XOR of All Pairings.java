1010d7b63066158d0145a8b046205fc98b461726
Jayant R Habbu <habbujayanth@gmail.com>
2025-01-16 09:51:48 +0530

    Create Bitwise XOR of All Pairings.java
    Example 1:
    
    Input: nums1 = [2,1,3], nums2 = [10,2,5,0]
    Output: 13
    Explanation:
    A possible nums3 array is [8,0,7,2,11,3,4,1,9,1,6,3].
    The bitwise XOR of all these numbers is 13, so we return 13.
    Example 2:
    
    Input: nums1 = [1,2], nums2 = [3,4]
    Output: 0
    Explanation:
    All possible pairs of bitwise XORs are nums1[0] ^ nums2[0], nums1[0] ^ nums2[1], nums1[1] ^ nums2[0],
    and nums1[1] ^ nums2[1].
    Thus, one possible nums3 array is [2,5,1,6].
    2 ^ 5 ^ 1 ^ 6 = 0, so we return 0.

---
+++ Arrays/Bit Manipulation/Bitwise XOR of All Pairings.java
@@ -0,0 +1,41 @@
+//Solution
+
+class Solution {
+    public int xorAllNums(int[] nums1, int[] nums2) {
+        int c1 = nums1.length;
+        int c2 = nums2.length;
+        int x1 = 0, x2 = 0;
+
+        if (c1 % 2 != 0) {
+            for (int num : nums2) {
+                x2 ^= num;
+            }
+        }
+        if (c2 % 2 != 0) {
+            for (int num : nums1) {
+                x1 ^= num;
+            }
+        }
+        return x1 ^ x2;
+    }
+}
+
+/*
+
+Example 1:
+Input: nums1 = [2,1,3], nums2 = [10,2,5,0]
+Output: 13
+Explanation:
+A possible nums3 array is [8,0,7,2,11,3,4,1,9,1,6,3].
+The bitwise XOR of all these numbers is 13, so we return 13.
+
+Example 2:
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 0
+Explanation:
+All possible pairs of bitwise XORs are nums1[0] ^ nums2[0], nums1[0] ^ nums2[1], nums1[1] ^ nums2[0],
+and nums1[1] ^ nums2[1].
+Thus, one possible nums3 array is [2,5,1,6].
+2 ^ 5 ^ 1 ^ 6 = 0, so we return 0.
+ 
+*/
