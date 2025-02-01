5a14b082441377c06fb19fff3b416071d7815c72
Jayant R Habbu <habbujayanth@gmail.com>
2025-02-01 08:55:53 +0530

    Create Special Array I.java
    An array is considered special if every pair of its adjacent elements contains two numbers with different parity.
    
    You are given an array of integers nums. Return true if nums is a special array, otherwise, return false.

---
+++ Arrays/Bit Manipulation/Special Array I.java
@@ -0,0 +1,33 @@
+//Solution
+
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            if (((nums[i] ^ nums[i-1]) & 1) == 0) 
+                return false;  
+        }
+        return true; 
+    }
+}
+
+/*
+
+Example 1:
+Input: nums = [1]
+Output: true
+Explanation:
+There is only one element. So the answer is true.
+
+Example 2:
+Input: nums = [2,1,4]
+Output: true
+Explanation:
+There is only two pairs: (2,1) and (1,4), and both of them contain numbers with different parity. So the answer is true.
+
+Example 3:
+Input: nums = [4,3,1,6]
+Output: false
+Explanation:
+nums[1] and nums[2] are both odd. So the answer is false.
+
+*/
