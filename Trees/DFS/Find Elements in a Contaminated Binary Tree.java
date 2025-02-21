f36faa87f12ebbbdd7c03ff86c45db6d1d9c1a3d
Jayant R Habbu <habbujayanth@gmail.com>
2025-02-21 08:20:27 +0530

    Create Find Elements in a Contaminated Binary Tree.java
    Given a binary tree with the following rules:
    
    root.val == 0
    For any treeNode:
    If treeNode.val has a value x and treeNode.left != null, then treeNode.left.val == 2 * x + 1
    If treeNode.val has a value x and treeNode.right != null, then treeNode.right.val == 2 * x + 2
    Now the binary tree is contaminated, which means all treeNode.val have been changed to -1.
    
    Implement the FindElements class:
    
    FindElements(TreeNode* root) Initializes the object with a contaminated binary tree and recovers it.
    bool find(int target) Returns true if the target value exists in the recovered binary tree.

---
+++ Trees/DFS/Find Elements in a Contaminated Binary Tree.java
@@ -0,0 +1,154 @@
+//Solution
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class FindElements {
+    TreeNode groot;
+
+    public FindElements(TreeNode root) {
+        if(root!=null && root.val==-1){
+            root.val=0;                
+        }
+        solve1(root);   
+        groot=root;  
+
+        //printTree(groot); //Used for printing the new Tree
+    }
+
+    void printTree(TreeNode node){
+        if(node==null){ 
+            System.out.print(" null ");
+            return;
+        }
+
+        System.out.print(" "+node.val);
+        printTree(node.left);
+        printTree(node.right);
+    }
+
+
+
+    void solve1(TreeNode node){
+        if(node==null){
+            return;
+        }
+
+        if(node.left !=null){
+            node.left.val=2*node.val+1;                
+        }
+        if(node.right !=null){
+            node.right.val=2*node.val+2;
+        }
+
+        solve1(node.left);
+        solve1(node.right);
+
+    }
+
+
+    
+    public boolean find(int target) {
+        TreeNode node=new TreeNode();
+        node=groot;
+        return solve2(node, target);
+    }
+
+    boolean solve2(TreeNode node, int target){
+        if(node==null){
+            return false;
+        }
+        if(node.val==target){
+            return true;
+        }
+        
+        if(solve2(node.left, target) || solve2(node.right, target)) return true;
+
+        return false;
+    }  
+
+  
+}
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements obj = new FindElements(root);
+ * boolean param_1 = obj.find(target);
+ */
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements obj = new FindElements(root);
+ * boolean param_1 = obj.find(target);
+ */
+
+
+
+
+
+/*
+
+Example 1:
+Input
+["FindElements","find","find"]
+[[[-1,null,-1]],[1],[2]]
+Output
+[null,false,true]
+Explanation
+FindElements findElements = new FindElements([-1,null,-1]); 
+findElements.find(1); // return False 
+findElements.find(2); // return True 
+
+Example 2:
+Input
+["FindElements","find","find","find"]
+[[[-1,-1,-1,-1,-1]],[1],[3],[5]]
+Output
+[null,true,true,false]
+Explanation
+FindElements findElements = new FindElements([-1,-1,-1,-1,-1]);
+findElements.find(1); // return True
+findElements.find(3); // return True
+findElements.find(5); // return False
+
+Example 3:
+Input
+["FindElements","find","find","find","find"]
+[[[-1,null,-1,-1,null,-1]],[2],[3],[4],[5]]
+Output
+[null,true,false,false,true]
+Explanation
+FindElements findElements = new FindElements([-1,null,-1,-1,null,-1]);
+findElements.find(2); // return True
+findElements.find(3); // return False
+findElements.find(4); // return False
+findElements.find(5); // return True
+ 
+*/
