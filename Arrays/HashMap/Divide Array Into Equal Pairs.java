3467423996566f1814bbd7d3b9a0abafa148e0f1
Jayant R Habbu <habbujayanth@gmail.com>
2025-03-17 08:14:40 +0530

    Create Divide Array Into Equal Pairs.java
    You are given an integer array nums consisting of 2 * n integers.
    
    You need to divide nums into n pairs such that:
    
    Each element belongs to exactly one pair.
    The elements present in a pair are equal.
    Return true if nums can be divided into n pairs, otherwise return false.

---
+++ Arrays/HashMap/Divide Array Into Equal Pairs.java
@@ -0,0 +1,35 @@
+//Solution
+
+class Solution {
+    public boolean divideArray(int[] nums) {
+        Map<Integer, Integer> counter = new HashMap<>();
+        for (int num : nums) {
+            counter.put(num, counter.getOrDefault(num, 0) + 1);
+        }
+        
+        for (int count : counter.values()) {
+            if (count % 2 != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
+
+/*
+
+Example 1:
+Input: nums = [3,2,3,2,2,2]
+Output: true
+Explanation: 
+There are 6 elements in nums, so they should be divided into 6 / 2 = 3 pairs.
+If nums is divided into the pairs (2, 2), (3, 3), and (2, 2), it will satisfy all the conditions.
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: false
+Explanation: 
+There is no way to divide nums into 4 / 2 = 2 pairs such that the pairs satisfy every condition.
+
+*/
