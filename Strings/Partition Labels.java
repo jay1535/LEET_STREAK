b0e53766a624f9073862875a53b6887df669c619
Jayant R Habbu <habbujayanth@gmail.com>
2025-03-30 08:37:49 +0530

    Create Partition Labels.java
    You are given a string s. We want to partition the string into as many parts as possible so that each letter appears in at most one part. For example, the string "ababcc" can be partitioned into ["abab", "cc"], but partitions such as ["aba", "bcc"] or ["ab", "ab", "cc"] are invalid.
    
    Note that the partition is done so that after concatenating all the parts in order, the resultant string should be s.
    
    Return a list of integers representing the size of these parts.

---
+++ Strings/Partition Labels.java
@@ -0,0 +1,39 @@
+//Solution
+
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        Map<Character, Integer> lastOccurrence = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            lastOccurrence.put(s.charAt(i), i);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int start = 0, end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            end = Math.max(end, lastOccurrence.get(s.charAt(i)));
+            if (i == end) {
+                result.add(end - start + 1);
+                start = i + 1;
+            }
+        }
+
+        return result;
+    }
+}
+
+/*
+
+Example 1:
+Input: s = "ababcbacadefegdehijhklij"
+Output: [9,7,8]
+Explanation:
+The partition is "ababcbaca", "defegde", "hijhklij".
+This is a partition so that each letter appears in at most one part.
+A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits s into less parts.
+
+Example 2:
+Input: s = "eccbbbbdec"
+Output: [10]
+
+*/
