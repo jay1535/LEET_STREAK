0cb799bbbcbcacfffb2550421b17c2306851cf93
Jayant R Habbu <habbujayanth@gmail.com>
2025-04-02 19:57:08 +0530

    Create Maximum Value of an Ordered Triplet I.java
    You are given a 0-indexed integer array nums.
    
    Return the maximum value over all triplets of indices (i, j, k) such that i < j < k. If all such triplets have a negative value, return 0.
    
    The value of a triplet of indices (i, j, k) is equal to (nums[i] - nums[j]) * nums[k].

---
+++ Arrays/Two Pointers/Maximum Value of an Ordered Triplet I.java
@@ -0,0 +1,39 @@
+//Solution
+
+class Solution {
+    public long maximumTripletValue(int[] nums) {
+        long maxTriplet = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int k = nums.length - 1; k > i; k--) {
+                int j = i + 1;
+                while (j < k) {
+                    maxTriplet = Math.max(maxTriplet, (long)(nums[i] - nums[j]) * nums[k]);
+                    j++;
+                }
+            }
+        }
+        return Math.max(0, maxTriplet);
+    }
+}
+
+
+/*
+
+Example 1:
+Input: nums = [12,6,1,2,7]
+Output: 77
+Explanation: The value of the triplet (0, 2, 4) is (nums[0] - nums[2]) * nums[4] = 77.
+It can be shown that there are no ordered triplets of indices with a value greater than 77. 
+
+Example 2:
+Input: nums = [1,10,3,4,19]
+Output: 133
+Explanation: The value of the triplet (1, 2, 4) is (nums[1] - nums[2]) * nums[4] = 133.
+It can be shown that there are no ordered triplets of indices with a value greater than 133.
+
+Example 3:
+Input: nums = [1,2,3]
+Output: 0
+Explanation: The only ordered triplet of indices (0, 1, 2) has a negative value of (nums[0] - nums[1]) * nums[2] = -3. Hence, the answer would be 0.
+
+*/
