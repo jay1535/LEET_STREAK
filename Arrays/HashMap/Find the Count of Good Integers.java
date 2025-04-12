31573f94d03cbffbe2ae59cf12feec20f0ef2b27
Jayant R Habbu <habbujayanth@gmail.com>
2025-04-12 10:58:55 +0530

    Create Find the Count of Good Integers.java
    You are given two positive integers n and k.
    
    An integer x is called k-palindromic if:
    
    x is a palindrome.
    x is divisible by k.
    An integer is called good if its digits can be rearranged to form a k-palindromic integer. For example, for k = 2, 2020 can be rearranged to form the k-palindromic integer 2002, whereas 1010 cannot be rearranged to form a k-palindromic integer.
    
    Return the count of good integers containing n digits.
    
    Note that any integer must not have leading zeros, neither before nor after rearrangement. For example, 1010 cannot be rearranged to form 101.

---
+++ Arrays/HashMap/Find the Count of Good Integers.java
@@ -0,0 +1,62 @@
+//Solution
+
+class Solution {
+
+    public long countGoodIntegers(int n, int k) {
+        Set<String> dict = new HashSet<>();
+        int base = (int) Math.pow(10, (n - 1) / 2);
+        int skip = n & 1;
+        for (int i = base; i < base * 10; i++) {
+            String s = Integer.toString(i);
+            s += new StringBuilder(s).reverse().substring(skip);
+            long palindromicInteger = Long.parseLong(s);
+            if (palindromicInteger % k == 0) {
+                char[] chars = s.toCharArray();
+                Arrays.sort(chars);
+                dict.add(new String(chars));
+            }
+        }
+
+        long[] factorial = new long[n + 1];
+        factorial[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial[i] = factorial[i - 1] * i;
+        }
+        long ans = 0;
+        for (String s : dict) {
+            int[] cnt = new int[10];
+            for (char c : s.toCharArray()) {
+                cnt[c - '0']++;
+            }
+            long tot = (n - cnt[0]) * factorial[n - 1];
+            for (int x : cnt) {
+                tot /= factorial[x];
+            }
+            ans += tot;
+        }
+
+        return ans;
+    }
+}
+
+/*
+
+Example 1:
+Input: n = 3, k = 5
+Output: 27
+Explanation:
+Some of the good integers are:
+551 because it can be rearranged to form 515.
+525 because it is already k-palindromic.
+
+Example 2:
+Input: n = 1, k = 4
+Output: 2
+Explanation:
+The two good integers are 4 and 8.
+
+Example 3:
+Input: n = 5, k = 6
+Output: 2468
+
+*/
