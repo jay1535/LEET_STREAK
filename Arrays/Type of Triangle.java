c9e13a5755b41abe4264d60d90da8e4cfd5c64f3
Jayant R Habbu <habbujayanth@gmail.com>
2025-05-19 07:45:03 +0530

    Create Type of Triangle.java
    You are given a 0-indexed integer array nums of size 3 which can form the sides of a triangle.
    
    A triangle is called equilateral if it has all sides of equal length.
    A triangle is called isosceles if it has exactly two sides of equal length.
    A triangle is called scalene if all its sides are of different lengths.
    Return a string representing the type of triangle that can be formed or "none" if it cannot form a triangle.

---
+++ Arrays/Type of Triangle.java
@@ -0,0 +1,30 @@
+//Solution
+
+class Solution {
+    public String triangleType(int[] nums) {
+        int a = nums[0], b = nums[1], c = nums[2];
+        if (a + b <= c || a + c <= b || b + c <= a) return "none";
+        if (a == b && b == c) return "equilateral";
+        if (a == b || b == c || a == c) return "isosceles";
+        return "scalene";
+    }
+}
+
+/*
+
+Example 1:
+Input: nums = [3,3,3]
+Output: "equilateral"
+Explanation: Since all the sides are of equal length, therefore, it will form an equilateral triangle.
+
+Example 2:
+Input: nums = [3,4,5]
+Output: "scalene"
+Explanation: 
+nums[0] + nums[1] = 3 + 4 = 7, which is greater than nums[2] = 5.
+nums[0] + nums[2] = 3 + 5 = 8, which is greater than nums[1] = 4.
+nums[1] + nums[2] = 4 + 5 = 9, which is greater than nums[0] = 3. 
+Since the sum of the two sides is greater than the third side for all three cases, therefore, it can form a triangle.
+As all the sides are of different lengths, it will form a scalene triangle.
+
+*/
