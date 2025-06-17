41915339372a40af1648ff2da74cde5fd9b8facd
Jayant R Habbu <habbujayanth@gmail.com>
2025-06-17 08:12:17 +0530

    Create Count the Number of Arrays with K Matching Adjacent Elements.java
    You are given three integers n, m, k. A good array arr of size n is defined as follows:
    
    Each element in arr is in the inclusive range [1, m].
    Exactly k indices i (where 1 <= i < n) satisfy the condition arr[i - 1] == arr[i].
    Return the number of good arrays that can be formed.
    
    Since the answer may be very large, return it modulo 109 + 7.

---
+++ Math/Count the Number of Arrays with K Matching Adjacent Elements.java
@@ -0,0 +1,62 @@
+//Solution
+
+class Solution {
+    int mod = 1_000_000_007;
+    static long[] revs = new long[100001];
+    static int[] f = new int[100001];
+    public int countGoodArrays(int n, int m, int k) {
+        if (f[0] == 0)
+            f[0] = 1;
+        long res = m * pow(m - 1, n - 1 - k) % mod * C(n - 1, n - 1 - k) % mod;
+        
+        return (int) res;
+    }
+    public long pow(int a, int b) {
+        long res = 1;
+        long base = a;
+        while (b > 0) {
+            if ((b & 1) == 1)
+                res = res * base % mod;
+            base = base * base % mod;
+            b /= 2;
+        }
+        return res;
+    }
+    public long C(int a, int b) {
+        return (long) getF(a) * rev(getF(a - b)) % mod * rev(getF(b)) % mod;
+    }
+    public long getF(int a) {
+        if (f[a] != 0)
+            return f[a];
+        return f[a] = (int) (getF(a - 1) * a % mod);
+    }
+    public long rev(long a) {
+        if (a == 1)
+            return a;
+        return mod - mod / a * rev(mod % a) % mod;
+    }
+}
+
+/*
+
+Example 1:
+Input: n = 3, m = 2, k = 1
+Output: 4
+Explanation:
+There are 4 good arrays. They are [1, 1, 2], [1, 2, 2], [2, 1, 1] and [2, 2, 1].
+Hence, the answer is 4.
+
+Example 2:
+Input: n = 4, m = 2, k = 2
+Output: 6
+Explanation:
+The good arrays are [1, 1, 1, 2], [1, 1, 2, 2], [1, 2, 2, 2], [2, 1, 1, 1], [2, 2, 1, 1] and [2, 2, 2, 1].
+Hence, the answer is 6.
+
+Example 3:
+Input: n = 5, m = 2, k = 0
+Output: 2
+Explanation:
+The good arrays are [1, 2, 1, 2, 1] and [2, 1, 2, 1, 2]. Hence, the answer is 2.
+
+*/
