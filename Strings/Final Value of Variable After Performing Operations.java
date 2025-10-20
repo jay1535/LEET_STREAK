e434c9470cf6731b22d066e08473268ce7473641
Jayant R Habbu <habbujayanth@gmail.com>
2025-10-20 07:58:53 +0530

    Create Final Value of Variable After Performing Operations.java
    There is a programming language with only four operations and one variable X:
    
    ++X and X++ increments the value of the variable X by 1.
    --X and X-- decrements the value of the variable X by 1.
    Initially, the value of X is 0.
    
    Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.

---
+++ Strings/Final Value of Variable After Performing Operations.java
@@ -0,0 +1,40 @@
+//Solution
+
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int X = 0;  // start with X = 0
+        
+        for (String op : operations) {
+            if (op.contains("+")) {
+                X++;   
+            } else {
+                X--;  
+            }
+        }
+        
+        return X;
+    }
+}
+
+/*
+
+Example 1:
+
+Input: operations = ["--X","X++","X++"]
+Output: 1
+Explanation: The operations are performed as follows:
+Initially, X = 0.
+--X: X is decremented by 1, X =  0 - 1 = -1.
+X++: X is incremented by 1, X = -1 + 1 =  0.
+X++: X is incremented by 1, X =  0 + 1 =  1.
+Example 2:
+
+Input: operations = ["++X","++X","X++"]
+Output: 3
+Explanation: The operations are performed as follows:
+Initially, X = 0.
+++X: X is incremented by 1, X = 0 + 1 = 1.
+++X: X is incremented by 1, X = 1 + 1 = 2.
+X++: X is incremented by 1, X = 2 + 1 = 3.
+
+*/
